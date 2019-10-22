e9aef7390f6a760fe3d636ad1a9c50cf19401996
shengjieguan <49515716+shengjieguan@users.noreply.github.com>
2019-10-22 12:27:21 -0700

    Add files via upload

--- fill/check_valid.c
+++ fill/check_valid.c
@@ -1,5 +1,40 @@
 #include "header.h"
 
+int		ft_match(char **ar, int x, int y)
+{
+	
+	while(x < 4)
+	{
+		if(ft_strchr(ar[x], '#') == NULL)
+			i++;
+		else
+		{
+			while(
+		
+
+int		ft_check(char *b)
+{
+	char c[21];
+	t_xy *c;
+	static char s[546];
+	s = b;
+	char ar[3][3]
+
+
+	while(*b)
+	{
+		ar[0] = ft_strsub(b, 0, 4);
+		ar[1] = ft_strsub(b, 5, 4);
+		ar[2] = ft_strsub(b, 10, 4);
+		ar[3] = ft_strsub(b, 15, 4);
+	   	if(ft_match(ar, 0, 0) == 0)
+			return (0);
+		free(ar);
+		*(b + 21);
+	}
+	return (1);
+}
+
 int	check_valid(int fd)
 {
 	int ret;
@@ -12,10 +47,17 @@ int	check_valid(int fd)
 	((ret + 1) % 21) ? return (0) : count = ((ret + 1) / 21) * 4;
 	while (ret--)
 	{
-		(b[ret] != '#' && b[ret] != '.' && b != '\n') && return (0);
-		(b[ret] == '#') && count--;
+		if((ret + 1) % 21 == 0 || ret % 5 == 4)
+			(b[ret] != '\n') && return (0);
+		else if(ret % 5 < 4)
+		{
+			(b[ret] != '#' && b[ret] != '.' && b != '\n') && return (0);
+			(b[ret] == '#') && count--;
+		}
 	}
 	(count != 0) && return (0);
+	return (ft_check(b));
+}
 	 
 		
 	
