6d3f9500d5d009dbd0d452c8f55e249eb124fbc2
shengjieguan <49515716+shengjieguan@users.noreply.github.com>
2019-10-24 00:40:51 -0700

    Add files via upload

--- solve.c
+++ solve.c
@@ -1,44 +1,33 @@
-void	delete_map(char **map, int size)
-{
-	int i;
+#include "../includes/fillit.h"
 
-	i = 0;
-	while (i < size)
-	{
-		ft_memdel((void **)&(map[i]));
-		i++;
-	}
-	ft_memdel((void **)&map);
-}
 
-void	print_map(char  **map, int size)
-{
-	int i;
 
-	i = 0;
-	while (i < size)
-	{
-		ft_putstr(map[i]);
-		ft_putchar('\n');
-		i++;
-	}
-}
+//void	print_map(char  **map, int size)
+//{
+//	int i;
 
-void	place(char *tetri_arr, char **map, char character)
-{
-	int i;
-	int x;
-	int y;
+//	i = 0;
+//	while (i < size)
+//	{
+//		ft_putstr(map[i]);
+//		ft_putchar('\n');
+//		i++;
+//	}
+//}
 
-	i = 0;
-	while (i <= 6)
-	{
-		x = tetri_arr[i] + a - 48;
-		y = tetri_arr[i + 1] + b - 48;
-		map[y][x] = character;
-		i += 2;
-	}
-}
+//void	delete_map(char **map, int size)
+//{
+//	int i;
+//
+//	i = 0;
+//	while (i < size)
+//	{
+//		ft_memdel((void **)&(map[i]));
+//		i++;
+//	}
+//	ft_memdel((void **)&map);
+//}
+
 int	overlap(char **map, char *tetri_arr, int a, int b)
 {
 	int i;
@@ -53,45 +42,60 @@ int	overlap(char **map, char *tetri_arr, int a, int b)
 		x = tetri_arr[i] + a - 48;
 		y = tetri_arr[i + 1] + b - 48;
 	}
-	(i == 8) : return (1) : return (0);
+	return(i == 8);
 }
 
-int		x_bounds(char *tetri_arr, int size)
+void	place(char *tetri_arr, char **map, char character, int a, int b)
+{
+	int i;
+	int x;
+	int y;
+
+	i = 0;
+	while (i <= 6)
+	{
+		x = tetri_arr[i] + a - 48;
+		y = tetri_arr[i + 1] + b - 48;
+		map[y][x] = character;
+		i += 2;
+	}
+}
+int		x_bounds(char *tetri_arr, int size, int x)
 {
 	return (tetri_arr[0] + x < size &&
 				tetri_arr[2] + x < size &&
 				tetri_arr[4] + x < size &&
 				tetri_arr[6] + x < size);	
 }
-int		y_bounds(char *tetri_arr, int size)
+int		y_bounds(char *tetri_arr, int size, int y)
 {
 	return (tetri_arr[1] + y < size &&
 				tetri_arr[3] + y < size &&
 				tetri_arr[5] + y < size &&
 				tetri_arr[7] + y < size);	
 }
-int recursion(char **tetri_arr, char **map, int size,)
+int recursion(char **tetri_arr, char **map, int size)
 {
 	int x;
 	int y;
 	int i;
-	char character[26];
+	char *character;
 
 	character = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	x = 0;
 	y = 0;
 	
-	while(y_bounds(tetri_arr[i], size))
+	while(y_bounds(tetri_arr[i], size, y))
 	{
-		while(x_bounds(tetri_arr[i], size)
+		while(x_bounds(tetri_arr[i], size, x))
 		{
-			if(overlap(map, tetri_arr[i], x, y)  == 1)
+			if(overlap(map, tetri_arr[i], x, y))
 			{
-				place(map, tetri_arr[i], character[i]);
-				if (recursion((tetri_arr[i + 1], map) == 1)
+				place(tetri_arr[i], map, character[i], x, y);
+				if (recursion((tetri_arr[i + 1], map, size) == 1)
 					return (1);
 				else
-					place(map, tetri_arr[i], '.');
+					place(tetri_arr[i], map, '.', x, y);
 			}
 			x++;
 		}
@@ -129,9 +133,9 @@ void	solve(char **tetri_arr, int tetri_count) //add solve(**tetri_arr, int tetri
 	while(recursion(tetri_arr, map,size) == 0)
 	{
 		size++;
-		delete_map(map, size);
+//		delete_map(map, size);
 		map = set_board(size);
 	}
-	print_map(map, size);
-	delete_map(map, size);
+//	print_map(map, size);
+//	delete_map(map, size);
 }
