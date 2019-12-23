3e703aed47aeaed3ed858ae0ed556692390b9869
shengjieguan <49515716+shengjieguan@users.noreply.github.com>
2019-12-23 12:01:32 -0800

    Update fdf.c

--- fdf.c
+++ fdf.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+mlx->map#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include "../includes/get_next_line.h"
 #include "../libft/libft.h"
 #include "../miniLibX/mlx.h"
-#include <string.h>
 #define WIN_WID 1000
 #define WIN_HEI 1000
 
@@ -23,16 +22,15 @@ typedef struct s_image
 	int bpp;
 	int endian;
 	int size;
-//	void	*ptr;
-//	char	*str;
+
 }				t_image;
 typedef struct s_mlx
 {
 	void	*mlx;
 	void	*window;
-	void	*img; // equel to *ptr
+	void	*img;
 	char	*data;
-	t_map 	*map[99999];
+	t_map 	*map[9999];
 	int		map_width;
 	int		map_height;
 	int		count_point;
@@ -49,26 +47,11 @@ void	init(t_mlx *mlx)
 	mlx->map_height = -1;
 	mlx->map_num_max = 0;
 	mlx->map_num_min = 0;
-}
 
-void	move_map_to_center(t_mlx *mlx)
-{
-	int i;
-
-	i = -1;
-	while(++i <= mlx->count_point)
-	{
-		mlx->map[i]->center_x = (WIN_WID - mlx->map_width / 2) + mlx->map[i]->x;
-		mlx->map[i]->center_y = (WIN_HEI - mlx->map_height / 2) + mlx->map[i]->y;
-
-		// if(mlx->map[i]->num >= 0 && mlx->map[i]->num >= mlx->map_num_max)
-		// 	mlx->map_num_max = mlx->map[i]->num;
-		// if(mlx->map[i]->num < 0 && mlx->map[i]->num < mlx->map_num_max)
-		// 	mlx->map_num_min = mlx->map[i]->num;
-	}
 
 }
-void swap(double *x, double *y)//使用指针传递地址
+
+void swap(double *x, double *y)
 {
 double temp;
 temp=*x;
@@ -80,25 +63,29 @@ temp=*x;
 void				plot(t_mlx *mlx, int x, int y)
 {
 	int				i;
-	int				dest;
-	unsigned int	new_color;
+	// int				dest;
+	// unsigned int	new_color;
 
-	if (y < 0 || x < 0)
-		return ;
-	new_color = mlx_get_color_value(mlx->img, 0xFFFFFF);
-	dest = y * mlx->image->size + x * (mlx->image->bpp / 8);
-	i = -1;
-	while (++i < mlx->image->bpp / 8)
-	{
-		if (mlx->image->endian == 0)
-			mlx->data[dest + i] = new_color >> (i * 8) & 0xFF;
-		else
-			mlx->data[dest + i] = new_color >> (mlx->image->bpp - (i + 1) * 8) & 0xFF;
-	}
+	// if (y < 0 || x < 0)
+	// 	return ;
+	// new_color = mlx_get_color_value(mlx->img, 0x00FFFF);
+	// dest = y * mlx->image->size + x * (mlx->image->bpp / 8);
+	// i = -1;
+	// while (++i < mlx->image->bpp / 8)
+	// {
+	// 	if (mlx->image->endian == 0)
+	// 		mlx->data[dest + i] = new_color >> (i * 8) & 0xFF;
+	// 	else
+	// 		mlx->data[dest + i] = new_color >> (mlx->image->bpp - (i + 1) * 8) & 0xFF;
+	// }
+	i = 0;
+	i = (x * 4) + (y * mlx->image->size);
+	mlx->data[i++] = (char)0;
+	mlx->data[i++] = (char)0;
+	mlx->data[i++] = (char)255;
 }
 
 void	line(t_map *cur, t_map *next, t_mlx *mlx)
-//oid	line(t_map *cur, t_map *next)
 {
 	double	dx;
 	double dy;
@@ -127,6 +114,7 @@ void	line(t_map *cur, t_map *next, t_mlx *mlx)
 	error = dx / 2;
 	y = cur->center_y;
 	x = cur->center_x;
+
 	//ystep = (next->center_y > cur->center_y ? 1 : -1);
 	while(next->center_x >= ++x)
 	{
@@ -140,28 +128,58 @@ void	line(t_map *cur, t_map *next, t_mlx *mlx)
 		}
 	}
 }
+void	move_map_to_center(t_mlx *mlx)
+{
+	int i;
+
+	i = -1;
+	while(++i <= mlx->count_point)
+	{
+		mlx->map[i]->center_x = (WIN_WID - mlx->map_width / 2) + mlx->map[i]->x;
+		mlx->map[i]->center_y = (WIN_HEI - mlx->map_height / 2) + mlx->map[i]->y;
+
+		// if(mlx->map[i]->num >= 0 && mlx->map[i]->num >= mlx->map_num_max)
+		// 	mlx->map_num_max = mlx->map[i]->num;
+		// if(mlx->map[i]->num < 0 && mlx->map[i]->num < mlx->map_num_max)
+		// 	mlx->map_num_min = mlx->map[i]->num;
+	}
+	
+}
+
+
 void	draw_line(t_mlx *mlx)
 {
 	
 	int i;
 
 	i = 0;
-	move_map_to_center(mlx);
-	
-	while (i <= mlx->count_point)
-	{
-		if(mlx->map[i]->x + 1 <= mlx->map_width)
+	//move_map_to_center(mlx);
+	// i = -1;
+	// while (++i < mlx->count_point)
+	// 	printf("%f\n", mlx->map[i]->center_x);
+	//printf("%d", mlx->count_point);
+	//while (i <= mlx->count_point)
+	// while (i < mlx->count_point)
+	// {
+		//if(mlx->map[i]->x + 1 <= mlx->map_width)
 			line(mlx->map[i], mlx->map[i + 1], mlx);
-		if(mlx->map[i]->y + 1 <= mlx->map_height)
+		//if(mlx->map[i]->y + 1 <= mlx->map_height)
 			line(mlx->map[i], mlx->map[i + mlx->map_width + 1], mlx);			
-		i++;
-	}
+	// 	i++;
+	// }
+
+	mlx_put_image_to_window(mlx->mlx, mlx->window, mlx->img, 0, 0);
 }
-t_map	*add_to_map(int num, int x, int y, t_map *map)
+t_map	*add_to_map(int num, int x, int y)
 {
+	t_map *map;
+
+	if(!(map = malloc(sizeof(t_map))))
+		return (NULL);
 	map->x = x;
 	map->y = y;
 	map->num = num;
+	
 	return (map);
 }
 int		read_file(t_mlx *mlx, int fd)
@@ -170,7 +188,6 @@ int		read_file(t_mlx *mlx, int fd)
 	char **s;
 	int i;
 	int j;
-	t_map map;
 
 	j = -1;
 	mlx->map_width = 0;
@@ -182,8 +199,7 @@ int		read_file(t_mlx *mlx, int fd)
 			return (0);
 		while(s[++i])
 		{
-			mlx->map[++j] = add_to_map(atoi(s[i]), i, mlx->map_height, &map);
-			printf("%d", map.num);
+			mlx->map[++j] = add_to_map(atoi(s[i]), i, mlx->map_height);
 		}
 		if(mlx->map_width == 0)
 			mlx->map_width = i;
@@ -201,7 +217,6 @@ int		main(int	ar, char **av)
 	int fd;
 
 	init(&mlx);
-	printf("%d", mlx.map_height);
 	if (ar < 2)
 	{
 		puts("not enough arguments");
